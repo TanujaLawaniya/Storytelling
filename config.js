41bcbb1913c1d05095c870f15a8c59d43097d806
Tanuja Lawaniya <62615222+TanujaLawaniya@users.noreply.github.com>
2021-08-26 04:20:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuzKmLyP6DiPpTUQCdXs3oyT4VKnL4G8s",
+  authDomain: "themes-28797.firebaseapp.com",
+  databaseURL: "https://themes-28797-default-rtdb.firebaseio.com",
+  projectId: "themes-28797",
+  storageBucket: "themes-28797.appspot.com",
+  messagingSenderId: "755810605892",
+  appId: "1:755810605892:web:2f2b1e159e0c7d3a131511",
+  measurementId: "G-KSV7EG85P5"
+};
